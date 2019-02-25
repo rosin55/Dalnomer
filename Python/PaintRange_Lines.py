c3968e21f710addf8379d6b9ee6447cd5475e36b
Rosin55 <asakhno@gmail.com>
2019-02-25 13:55:55 +0300

    Удалил лишние import

--- Python/PaintRange_Lines.py
+++ Python/PaintRange_Lines.py
@@ -1,40 +1,40 @@
 # Рисует график расстояний, измеренных УЗ дальномером
 # и переданным в массив MassivTochek
 # с помощью Lines(x, y)
-# 27.01.2019
+# создан 27.01.2019
 
 import pygame
 import math
-import sys, glob, time, serial, os, struct, subprocess, threading
+import sys, serial, time#, glob, os, struct, subprocess, threading
 
 def serial_ports():
-    # Возвращает список доступных СОМ портов
-    """ Lists serial port names
+	# Возвращает список доступных СОМ портов
+	""" Lists serial port names
 
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
+		:raises EnvironmentError:
+			On unsupported or unknown platforms
+		:returns:
+			A list of the serial ports available on the system
+	"""
+	if sys.platform.startswith('win'):
+		ports = ['COM%s' % (i + 1) for i in range(256)]
+	elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+		# this excludes your current terminal "/dev/tty"
+		ports = glob.glob('/dev/tty[A-Za-z]*')
+	elif sys.platform.startswith('darwin'):
+		ports = glob.glob('/dev/tty.*')
+	else:
+		raise EnvironmentError('Unsupported platform')
 
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
+	result = []
+	for port in ports:
+		try:
+			s = serial.Serial(port)
+			s.close()
+			result.append(port)
+		except (OSError, serial.SerialException):
+			pass
+	return result
 
 def PaintSimbol( s, x, y):
 	text = font.render(s, 1, (COLOR_TEXT))
